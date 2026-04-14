240cde0404bb2fbe2556bc96b67efb6f11c37660
Clémentine BERAUD <clementine.beraud@icloud.com>
2026-04-14 11:33:06 +0200

    je commit

---
+++ js/quiz.js
@@ -0,0 +1,50 @@
+let tentative=1;
+
+function submitQuiz(){
+    let score=0;
+    text=document.getElementById("texte").value.toLowerCase()
+    if (document.getElementById("radio1").checked){
+        score=score+3;
+    }
+    if (document.getElementById("radio5").checked){
+        score=score+3;
+    }
+    if (document.getElementById("radio7").checked){
+        score=score+3;
+    }
+    if (document.getElementById("checkbox1").checked){
+        score=score+1.5;
+    }
+    if (document.getElementById("checkbox2").checked){
+        score=score+1.5;
+    }
+    if (document.getElementById("checkbox3").checked){
+        score=score-1.5;
+    }
+  
+
+    let ligne= document.createElement("tr");
+    
+    let e = document.createElement("td");
+    e.textContent = tentative;
+    let s = document.createElement("td");
+    s.textContent = score;
+    ligne.appendChild(e);
+    ligne.appendChild(s);
+    document.querySelector("tbody").appendChild(ligne);
+    tentative++;
+
+    document.getElementById("radio1").checked = false;
+    document.getElementById("radio2").checked = false;
+    document.getElementById("radio3").checked = false;
+    document.getElementById("checkbox1").checked = false;
+    document.getElementById("checkbox2").checked = false;
+    document.getElementById("checkbox3").checked = false;
+    document.getElementById("texte").value = "";
+
+    if (tentative > 3) {
+        let bouton = document.getElementById("soumettre");
+        bouton.disabled = true;
+
+    }
+}
